ad39266724fa243da14524a9fa3ecbc0578300da
Andrew Sweeney <asweeney86@gmail.com>
2017-12-21 14:56:29 -0500

    Add an_array_set_allocator test (#9)
    * Add an_array_set_allocator test
    * Use exported function for accessing the default allocator
    * Validate that allocator was actually used
    * Switch to malloc for valgrind testing

--- test/check_an_array.c
+++ test/check_an_array.c
@@ -13,6 +13,54 @@ struct node {
 AN_ARRAY(node, test);
 AN_ARRAY_PRIMITIVE(int, test2);
 
+static struct allocator_stats {
+	size_t n_malloc;
+	size_t n_calloc;
+	size_t n_realloc;
+	size_t n_free;
+} allocator_stats;
+
+static void *
+check_calloc(const void *ctx, size_t nmemb, size_t size, void *return_addr)
+{
+	(void)ctx;
+	(void)return_addr;
+
+	allocator_stats.n_calloc++;
+	return calloc(nmemb, size);
+}
+
+static void *
+check_malloc(const void *ctx, size_t size, void *return_addr)
+{
+	(void)ctx;
+	(void)return_addr;
+
+	allocator_stats.n_malloc++;
+	return malloc(size);
+}
+
+static void *
+check_realloc(const void *ctx, void *address, size_t size_from, size_t size_to, void *return_addr)
+{
+	(void)ctx;
+	(void)size_from;
+	(void)return_addr;
+
+	allocator_stats.n_realloc++;
+	return realloc(address, size_to);
+}
+
+static void
+check_free(const void *ctx, void *ptr, void *return_addr)
+{
+	(void)ctx;
+	(void)return_addr;
+
+	allocator_stats.n_free++;
+	free(ptr);
+}
+
 START_TEST(stack_generic)
 {
 	AN_ARRAY_INSTANCE(test) array;
@@ -47,6 +95,8 @@ START_TEST(stack_generic)
 
 	AN_ARRAY_PUSH(test, &array, &entry[0]);
 	fail_if(array.capacity != 1024);
+
+	AN_ARRAY_DEINIT(test, &array);
 }
 END_TEST
 
@@ -72,6 +122,8 @@ START_TEST(stack)
 	int x = 0;
 	AN_ARRAY_PUSH(test2, &array, &x);
 	fail_if(array.capacity != 1024);
+
+	AN_ARRAY_DEINIT(test2, &array);
 }
 END_TEST
 
@@ -96,6 +148,8 @@ START_TEST(find_element)
 		fail_if(!AN_ARRAY_MEMBER(test2, &array, &i), "Could not find a valid member");
 	}
 	fail_if(AN_ARRAY_MEMBER(test2, &array, &x), "Found a member that does not belong");
+
+	AN_ARRAY_DEINIT(test2, &array);
 }
 END_TEST
 
@@ -113,6 +167,8 @@ START_TEST(remove_element)
 		fail_if(AN_ARRAY_MEMBER(test2, &array, &i), "Found a deleted member");
 		fail_if((int)AN_ARRAY_LENGTH(test2, &array) != 1023-i,"Remove member failed");
 	}
+
+	AN_ARRAY_DEINIT(test2, &array);
 }
 END_TEST
 
@@ -160,6 +216,9 @@ START_TEST(remove_order)
 	    "Previous element [%d] = %d is incorrect", to_remove_order - 1, to_remove_order - 1);
 	fail_if(*AN_ARRAY_VALUE(test2, &array, to_remove_order) != (to_remove_order + 1),
 	    "Next element [%d] = %d is incorrect", to_remove_order, to_remove_order + 1);
+
+
+	AN_ARRAY_DEINIT(test2, &array);
 }
 END_TEST
 
@@ -175,6 +234,32 @@ START_TEST(resize)
 }
 END_TEST
 
+START_TEST(allocator)
+{
+	struct an_allocator check_allocator = {
+		.malloc = check_malloc,
+		.calloc = check_calloc,
+		.realloc = check_realloc,
+		.free = check_free
+	};
+
+	memset(&allocator_stats, 0, sizeof(allocator_stats));
+
+	an_array_set_allocator(&check_allocator);
+
+	AN_ARRAY_INSTANCE(test2) array;
+	AN_ARRAY_INIT(test2, &array, 2);
+	AN_ARRAY_RESIZE(test2, &array, 128);
+	AN_ARRAY_DEINIT(test2, &array);
+
+	ck_assert(allocator_stats.n_calloc + allocator_stats.n_malloc > 0);
+	ck_assert(allocator_stats.n_realloc > 0);
+	ck_assert(allocator_stats.n_free > 0);
+
+	an_array_set_allocator(an_default_allocator());
+}
+END_TEST
+
 int
 main(int argc, char *argv[])
 {
@@ -182,6 +267,7 @@ main(int argc, char *argv[])
 	Suite *suite = suite_create("an_array");
 	TCase *tc = tcase_create("test_an_array");
 
+	tcase_add_test(tc, allocator);
 	tcase_add_test(tc, stack);
 	tcase_add_test(tc, stack_generic);
 	tcase_add_test(tc, find_element);
